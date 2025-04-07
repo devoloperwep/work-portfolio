2556d2c4ae1ead104d54eb1b31cc5433d4f33b69
devoloperwep <wepdevoloper@gmail.com>
2025-04-07 18:54:33 +0500

    delete example

--- js/data.js
+++ js/data.js
@@ -5,11 +5,6 @@ export default [
     vercel: "https://62-dars.vercel.app/",
   },
   {
-    title: "Example 2",
-    github: "https://github.com/devoloperwep/6.2-dars",
-    vercel: "https://62-dars.vercel.app/",
-  },
-  {
     title: "Example 1",
     github: "https://github.com/devoloperwep/6.1-dars",
     vercel: "https://6-1-dars-lovat.vercel.app/",
