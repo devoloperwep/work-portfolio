c2f5ba1c25748f9443020f85419c85a36f76e5bf
devoloperwep <wepdevoloper@gmail.com>
2025-04-07 18:53:48 +0500

    new exmaple

--- js/data.js
+++ js/data.js
@@ -5,6 +5,11 @@ export default [
     vercel: "https://62-dars.vercel.app/",
   },
   {
+    title: "Example 2",
+    github: "https://github.com/devoloperwep/6.2-dars",
+    vercel: "https://62-dars.vercel.app/",
+  },
+  {
     title: "Example 1",
     github: "https://github.com/devoloperwep/6.1-dars",
     vercel: "https://6-1-dars-lovat.vercel.app/",
