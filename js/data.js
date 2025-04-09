0634553acb9b9ff8c14c6719ef6313d82cbb88bf
devoloperwep <wepdevoloper@gmail.com>
2025-04-09 17:38:39 +0500

    new project

--- js/data.js
+++ js/data.js
@@ -1,5 +1,10 @@
 export default [
   {
+    title: "intro-section-with-dropdown-navigation",
+    github: "https://github.com/devoloperwep/intro-section",
+    vercel: "https://intro-section-flame-gamma.vercel.app/",
+  },
+  {
     title: "Example 2",
     github: "https://github.com/devoloperwep/6.2-dars",
     vercel: "https://62-dars.vercel.app/",
