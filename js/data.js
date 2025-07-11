4e0cd77894fa6f247699d6c96d2dfa5b634f4627
devoloperwep <wepdevoloper@gmail.com>
2025-07-11 11:48:55 +0500

    .

--- js/data.js
+++ js/data.js
@@ -1,5 +1,10 @@
 export default [
   {
+    title: "5-oy 10-vazifa products",
+    github: "https://github.com/devoloperwep/5.10-vazifa",
+    vercel: "https://5-10-vazifa.vercel.app/",
+  },
+  {
     title: "5-oy 9-vazifa products",
     github: "https://github.com/devoloperwep/5.9-vazifa",
     vercel: "https://5-9-vazifa-two.vercel.app/",
